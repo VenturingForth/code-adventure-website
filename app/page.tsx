965aaa548fd5c8323bbc9651820bf36cff35db18
Joshua Robinson <joshua@codeadventure.uk>
2024-06-07 13:25:33 +0100

    Initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Header from "./ui/header";
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+    <main>
+      <p>The home of Joshua Robinson's coding adventure, a junior full-stack developer looking for his first major role...</p>
+      <h2>Why Code Adventure?</h2>
+      <p></p>
+    </main>
+    </>
+  );
+}
---
+++ app/ui/header.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Header(){
+
+    return (
+        <header>
+            <h1 className="bg-teal-500">Code Adventure</h1>
+            <p className="bg-red-500">The home of Joshua Robinson's coding journey.</p>
+        </header>
+    )
+}
