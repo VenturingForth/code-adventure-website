b363918472cae93b1258adfbde04ecea86378fe2
Joshua Robinson <joshua@codeadventure.uk>
2024-08-13 10:27:12 +0100

    Moved intro and contents into their own section tags

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,16 @@ export default function Home() {
     <NavBar />
     <main>
     <h1 className="text-2xl font-bold text-teal-500">I'm Josh, and welcome to the home of my coding adventure!</h1>
-      <p>I've worked as a video and broadcast engineer for nearly twenty years, and I enjoyed writing code so much I decided to make a career of it and chartered a course into the world of software engineering. I wanted a place where I could chronicle my journey as a career-switching coder, and keep track of my projects and knowledge over the coming years. As such, this little den of knowledge includes links to completed projects, course progress and blog articles that I've written up. It is also a place fellow coders can contact me whether for employment opportunities, provide me with tips or advice, or even consult me on my own experiences if looking for advice of their own.</p>
-      <ul>Here, you'll find:
-        <li>My blog detailing my coding adventure</li>
-        <li>Projects I've worked on</li>
-        <li>Associated code repositories</li>
-        <li>My most current curriculum vitae</li>
-        <li>Social media links and profiles</li>
-      </ul>
+      <section>I've worked as a video and broadcast engineer for nearly twenty years, and I enjoyed writing code so much I decided to make a career of it and chartered a course into the world of software engineering. I wanted a place where I could chronicle my journey as a career-switching coder, and keep track of my projects and knowledge over the coming years. As such, this little den of knowledge includes links to completed projects, course progress and blog articles that I've written up. It is also a place fellow coders can contact me whether for employment opportunities, provide me with tips or advice, or even consult me on my own experiences if looking for advice of their own.</section>
+      <section>
+        <ul>Here, you'll find:
+          <li>My blog detailing my coding adventure</li>
+          <li>Projects I've worked on</li>
+          <li>Associated code repositories</li>
+          <li>My most current curriculum vitae</li>
+          <li>Social media links and profiles</li>
+        </ul>
+      </section>
       <Contact />
     </main>
     <footer>
